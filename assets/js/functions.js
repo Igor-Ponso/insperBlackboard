2bd474b27be0428b95dd000aece45b4ce92fc1d6
Igor Ponso <igorponso07@gmail.com>
2022-03-25 17:35:33 -0300

    new audio section + css modifications

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,16 @@
-function sendTo(id){
-    var elem = document.getElementById(id);
-    elem.scrollIntoView();
-
+function sendTo(id) {
+  var elem = document.getElementById(id);
+  elem.scrollIntoView();
 }
+
+window.addEventListener(
+  "resize",
+  function () {
+    var nav = document.getElementById("globalNavPageNavArea").getHeight();
+    var innerHeight = window.innerHeight;
+    var value = innerHeight - nav;
+    var elem = document.getElementById("newTheme");
+    elem.style.height = value;
+  },
+  true
+);
